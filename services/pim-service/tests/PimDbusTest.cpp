316a324cf4b88afb13a16e771be24928ae555dd2
Arcment <arcment@126.com>
2026-08-30 19:43:08 +0800

    test(pim): verify reminder notification delivery

--- services/pim-service/tests/PimDbusTest.cpp
+++ services/pim-service/tests/PimDbusTest.cpp
@@ -30,11 +30,28 @@ QString encode(const QJsonObject &object)
 
 } // namespace
 
+class FakeNotifications : public QObject {
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "org.freedesktop.Notifications")
+
+public slots:
+    uint Notify(const QString &, uint, const QString &, const QString &summary,
+                const QString &body, const QStringList &, const QVariantMap &, int)
+    {
+        emit notified(summary, body);
+        return 1;
+    }
+
+signals:
+    void notified(const QString &summary, const QString &body);
+};
+
 class PimDbusTest : public QObject {
     Q_OBJECT
 
 private slots:
     void servicePublishesMutations();
+    void serviceDeliversReminder();
 };
 
 void PimDbusTest::servicePublishesMutations()
@@ -97,6 +114,58 @@ void PimDbusTest::servicePublishesMutations()
         service.kill();
         QVERIFY(service.waitForFinished(3000));
     }
+}
+
+void PimDbusTest::serviceDeliversReminder()
+{
+    const QString serviceExecutable = qEnvironmentVariable("KOS_PIM_TEST_SERVICE");
+    QVERIFY2(!serviceExecutable.isEmpty(), "PIM service executable environment is missing");
+    QTemporaryDir storage;
+    QVERIFY(storage.isValid());
+
+    FakeNotifications notifications;
+    QDBusConnection bus = QDBusConnection::sessionBus();
+    QVERIFY(bus.registerObject(QStringLiteral("/org/freedesktop/Notifications"),
+                               &notifications, QDBusConnection::ExportAllSlots));
+    QVERIFY(bus.registerService(QStringLiteral("org.freedesktop.Notifications")));
+    QSignalSpy notifiedSpy(&notifications, &FakeNotifications::notified);
+
+    QProcess service;
+    QProcessEnvironment environment = QProcessEnvironment::systemEnvironment();
+    environment.insert(QStringLiteral("KOS_PIM_STORAGE_DIR"), storage.path());
+    service.setProcessEnvironment(environment);
+    service.setProgram(serviceExecutable);
+    service.start();
+    QVERIFY(service.waitForStarted(5000));
+
+    QDBusConnectionInterface *busInterface = bus.interface();
+    QVERIFY(busInterface);
+    QTRY_VERIFY_WITH_TIMEOUT(
+        busInterface->isServiceRegistered(QString::fromLatin1(serviceName)).value(), 5000);
+    QDBusInterface pim(QString::fromLatin1(serviceName), QString::fromLatin1(objectPath),
+                       QString::fromLatin1(interfaceName), bus);
+    QVERIFY(pim.isValid());
+
+    const QDateTime start = QDateTime::currentDateTimeUtc().addSecs(1);
+    const QDBusReply<QString> createReply = pim.call(
+        QStringLiteral("createEvent"), encode({
+            {QStringLiteral("title"), QStringLiteral("Reminder fixture")},
+            {QStringLiteral("start"), start.toString(Qt::ISODateWithMs)},
+            {QStringLiteral("end"), start.addSecs(3600).toString(Qt::ISODateWithMs)},
+            {QStringLiteral("reminderMinutes"), 0},
+        }));
+    QVERIFY2(createReply.isValid(), qPrintable(createReply.error().message()));
+    QVERIFY(parseObject(createReply.value()).value(QStringLiteral("ok")).toBool());
+    QTRY_COMPARE_WITH_TIMEOUT(notifiedSpy.count(), 1, 5000);
+    QCOMPARE(notifiedSpy.first().at(0).toString(), QStringLiteral("Reminder fixture"));
+
+    service.terminate();
+    if (!service.waitForFinished(3000)) {
+        service.kill();
+        QVERIFY(service.waitForFinished(3000));
+    }
+    bus.unregisterService(QStringLiteral("org.freedesktop.Notifications"));
+    bus.unregisterObject(QStringLiteral("/org/freedesktop/Notifications"));
 }
 
 QTEST_GUILESS_MAIN(PimDbusTest)
